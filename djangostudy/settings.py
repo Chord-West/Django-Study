f2e20e0656bdfbe8d4e57988b21202a4cae8562b
KimHyunSeo <gustj2005@naver.com>
2020-09-09 00:26:19 +0900

    update template index,html, result.html, select.html

--- djangostudy/settings.py
+++ djangostudy/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path,os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static")
+]
--- first/views.py
+++ first/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+from datetime import datetime
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    now = datetime.now()
+    context={
+        'current_date' : now
+    }  # 동적으로 삽입가능하게 하기위한 object
+    return render(request,'first/index.html',context)
 
 
 def select(request):
-    message = "수 하나를 입력해주세요."
-    return HttpResponse(message)
+    context={
+        'number' : 4
+    } 
+    return render(request,'first/select.html',context)
 
 
 def result(request):
-    message = "추첨 결과입니다."
-    return HttpResponse(message)
+    context={ 'numbers' : [1,2,3,4,5,6]
+    } 
+    return render(request,'first/result.html',context)
